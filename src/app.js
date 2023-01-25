167210f52d9d3141287717f3ea6eea441d02de03
nyamekye17 <122738782+nyamekye17@users.noreply.github.com>
2023-01-25 11:52:41 -1200

    updated the date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,22 @@
+function formatDate(timestamp){ 
+    let date = new Date(timestamp); 
+    let hours = date.getHours(); 
+    if (hours<10){ 
+      hours = `0${hours}`; 
+    } 
+   
+    let minutes = date.getMinutes(); 
+    if (minutes<10){ 
+      minutes = `0${minutes}`; 
+    } 
+   
+     let days = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", ]; 
+    let day = days[date.getDay()]; 
+   
+   return `${day} ${hours}:${minutes}`; 
+   
+   } 
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,10 +28,12 @@ function displayTemperature(response){
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector("#wind");
     windElement.innerHTML = Math.round(response.data.wind.speed);
-   
+    let dateElement=document.querySelector("#date"); 
+    dateElement.innerHTML= formatDate(response.data.dt*1000); 
 }
 
 let apiKey = "e97ae5d675e4c0ea5fe7521c6da29471";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Seoul&appid=${apiKey}&units=metric`;
+let city = "Accra"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature)
