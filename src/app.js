0f2f398ea51e6cffeb471bac4f868db923e76c3b
nyamekye17 <122738782+nyamekye17@users.noreply.github.com>
2023-02-04 09:41:22 -1200

    display forecast for other days using API

--- src/app.js
+++ src/app.js
@@ -17,25 +17,36 @@ function formatDate(timestamp){
    
    } 
 
+   function formatDay(timestamp){
+    let date = new Date(timestamp * 1000);
+    let days = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", ]; 
+    let day = date.getDay(); 
+
+    return days[day];
+   }
+
+
    function displayForecast(response){
-    console.log(response.data.daily);
+   let forecast = response.data.daily;
 
     let forecastElement =document.querySelector("#forecast");
-    let days = ["Fri", "Sat",  "Sun", "Mon", "Tue", "Wed"]; 
+  
     let forecastHTML = `<div class="row">`;
-    days.forEach(function(day) {
+    forecast.forEach(function (forecastDay, index) {
+      if (index === 1 || index === 2 || index === 3 || index === 4 || index === 5 ||index === 6 ) {
       forecastHTML =  forecastHTML + `
       <div class="col-2">
           <div class="weather-forecast-date">
-     ${day}
+     ${formatDay(forecastDay.time)}
   </div>
-       <img class="image-prediction" src="image/sunny.cloudy.png">
+       <img class="image-forecast" src= ${forecastDay.condition.icon_url}>
        <div class="weather-forecast-temperature">
-          <span class="weather-forecast-max">31°</span>
-       <span class="weather-forecast-min">23°</span> 
+          <span class="weather-forecast-max">${Math.round(forecastDay.temperature.maximum)}°</span>
+       <span class="weather-forecast-min">${Math.round(forecastDay.temperature.minimum)}°</span> 
       </div>
       </div>    
       `;
+    }
     })
   
     forecastHTML = forecastHTML + `</div>`;
